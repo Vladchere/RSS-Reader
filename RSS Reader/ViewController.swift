819ffc8973541e38a12d03a836b1ba14fb865291
Vladchere <vladchere.ok@mail.ru>
2021-05-25 21:52:26 +0700

    Initial Commit

---
+++ RSS Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSS Reader
+//
+//  Created by Vladislav Cheremisov on 25.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
